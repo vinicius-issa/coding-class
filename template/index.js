1986abe9c025ecbe3a815fd06e858fbe2f1f31c0
Vinicius de Almeida Issa <vinicius.almeidaissa@gmail.com>
2021-04-17 08:56:46 -0300

    com exemplo

--- template/index.js
+++ template/index.js
@@ -1,1 +1,5 @@
-console.log("Hello World")
+let nome = prompt("Qual o seu nome")
+let sobrenome = prompt("Qual o seu sobrenome")
+
+alert(nome + " " + sobrenome)
+console.log(nome + " " + sobrenome)
